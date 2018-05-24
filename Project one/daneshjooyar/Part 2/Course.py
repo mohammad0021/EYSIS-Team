176cf6c32f201a8aed9a56486852876b67718e75
mohammad0021 <34937551+mohammad0021@users.noreply.github.com>
2018-05-24 10:53:15 +0430

    Update Course.py

--- Project one/daneshjooyar/Part 2/Course.py
+++ Project one/daneshjooyar/Part 2/Course.py
@@ -3,9 +3,10 @@ import re
 import urllib
 import urllib2
 
+Link = "https://www.daneshjooyar.com/category/%d8%a2%d9%85%d9%88%d8%b2%d8%b4-%d9%87%d8%a7%db%8c-%d9%85%d8%af%d8%b1%d8%b3%db%8c%d9%86/%d9%85%d8%af%d8%b1%d8%b3-%d8%a7%da%a9%d8%a8%d8%b1%db%8c-%d9%85%d9%86%d8%b4/"
 opener = urllib2.build_opener()
 opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-state = opener.open("https://www.daneshjooyar.com")
+state = opener.open(Link)
 
 code = state.read()
 
