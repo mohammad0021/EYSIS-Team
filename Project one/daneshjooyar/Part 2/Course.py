e3a4f6d53c03cbb9e38d169762033ac61edd9f47
mohammad0021 <34937551+mohammad0021@users.noreply.github.com>
2018-05-24 10:38:50 +0430

    Add files via upload

---
+++ Project one/daneshjooyar/Part 2/Course.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import re
+import urllib
+import urllib2
+
+opener = urllib2.build_opener()
+opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
+state = opener.open("https://www.daneshjooyar.com")
+
+code = state.read()
+
+Course = re.findall('''"(.+)" class="col-lg-3 col-sm-4 col-xs-12''',code,re.M)
+
+f = open('Course.Link','w')
+
+for i in Course:
+	f.write(i)
